d2069f771c74aeb542db1d8c5ead57198fd9b29e
Zack Brown <zackrbrown@gmail.com>
2019-03-29 18:42:18 -0500

    added postgres stuff

---
+++ src/main/java/model/BlogPost.java
@@ -0,0 +1,58 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "blog_posts")
+public class BlogPost {
+
+    @Id
+    private UUID uuid;
+
+    @NotBlank
+    private String title;
+
+    @Column
+    private String content;
+
+    @NotBlank
+    private LocalDate createdDate;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+}
