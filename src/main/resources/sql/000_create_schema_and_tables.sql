da1d2dadb8ea7c62470a3d3e323b8e5b4981b7d9
Zack Brown <zack@pop-os.localdomain>
2019-04-21 20:00:31 -0500

    updated sql creation

--- src/main/resources/sql/000_create_schema_and_tables.sql
+++ src/main/resources/sql/000_create_schema_and_tables.sql
@@ -1,12 +1,20 @@
-CREATE SCHEMA IF NOT EXISTS zackrbrown_com;
+CREATE SCHEMA IF NOT EXISTS application;
 
-SET SEARCH_PATH TO zackrbrown_com;
+SET SEARCH_PATH TO application;
 
 CREATE TABLE IF NOT EXISTS posts(
     id UUID PRIMARY KEY,
     url_name VARCHAR(105), -- index
     title VARCHAR(100),
-    content VARCHAR(5000)
+    content VARCHAR(5000),
+    created_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS post_updates(
+    id UUID PRIMARY KEY,
+    post_id UUID,
+    updated_date DATE,
+    FOREIGN KEY(post_id) REFERENCES posts(id)
 );
 
 CREATE TABLE IF NOT EXISTS tags(
