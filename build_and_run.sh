87a66d0d98b05476a1c1504fab46b189d810464d
Zack Brown <zackrbrown@gmail.com>
2019-10-12 23:22:26 -0500

    MINDY-4 run docker in background

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker run \
+-d \
 --mount "type=bind,src=/etc/mindy/config,dst=/app/config" \
 --mount "type=bind,src=${PWD}/src/main/resources/static,dst=/app/static" \
 -p 443:443 -p 80:80 zackrbrown
