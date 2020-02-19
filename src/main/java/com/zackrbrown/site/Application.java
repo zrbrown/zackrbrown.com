ce39107e14f5fa0bba3f00c783a4cc0e95284f30
Zack Brown <zackrbrown@gmail.com>
2020-02-18 22:37:56 -0600

    added friendlyssl config and updated dependencies

--- src/main/java/com/zackrbrown/site/Application.java
+++ src/main/java/com/zackrbrown/site/Application.java
@@ -2,11 +2,13 @@ package com.zackrbrown.site;
 
 import net.eightlives.mindy.annotation.MindyApp;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @MindyApp
-@SpringBootApplication
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
